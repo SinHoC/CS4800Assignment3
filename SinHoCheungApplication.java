6f3632e5af20adeaca68c51b19eb9e70d4fd76ef
SinHoC <89657809+SinHoC@users.noreply.github.com>
2022-10-04 23:53:30 -0700

    Add files via upload

---
+++ SinHoCheungApplication.java
@@ -0,0 +1,21 @@
+package edu.TeamTemple.SinHo.Cheung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication	
+@RestController
+public class SinHoCheungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinHoCheungApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello! Welcome to SinHo's homepage";
+	}
+	
+}
